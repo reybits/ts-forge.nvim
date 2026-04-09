c2ed9d5ce8592f370cce822b4f62ae31307199a8
Andrey A. Ugolnik <andrey@ugolnik.info>
2026-04-09 14:42:55 +0200

    refactor: rename ts-parsers to ts-forge
    Renamed module and plugin files from ts-parsers to ts-forge and updated
    all require statements accordingly for consistency.

--- plugin/ts-forge.lua
+++ plugin/ts-forge.lua
@@ -1,15 +1,15 @@
 vim.api.nvim_create_user_command("TSInstall", function(args)
-    require("ts-parsers").install(#args.fargs > 0 and args.fargs or nil)
+    require("ts-forge").install(#args.fargs > 0 and args.fargs or nil)
 end, {
     nargs = "*",
     complete = function()
-        return vim.tbl_keys(require("ts-parsers").parsers)
+        return vim.tbl_keys(require("ts-forge").parsers)
     end,
     desc = "Install tree-sitter parsers",
 })
 
 vim.api.nvim_create_user_command("TSUpdate", function()
-    require("ts-parsers").update()
+    require("ts-forge").update()
 end, {
     nargs = 0,
     desc = "Update tree-sitter parsers to pinned revisions",
